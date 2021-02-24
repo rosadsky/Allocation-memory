be178f0c9cb57f1a3bc5f1fda9f250863ecd0598
rosadsky <rosadsky@hotmail.com>
2021-02-24 16:45:04 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/zadanie_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie_1.dir/main.c.o"
+  "zadanie_1"
+  "zadanie_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zadanie_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void *memory_alloc(unsigned int size);
+int memory_free(void *valid_ptr);
+int memory_check(void *ptr);
+void memory_init(void *ptr, unsigned int size);
+
+
+int main()
+{
+    printf("Ahoj !");
+
+
+    return 0;
+
+}
+
+void *memory_alloc(unsigned int size){
+
+
+}
+
+
