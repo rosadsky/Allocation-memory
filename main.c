268cd221bd9c8e1a133bd501e3b85129b557e0f2
rosadsky <rosadsky@hotmail.com>
2021-03-03 21:03:17 +0100

    pridavam memory free

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void *ukazovatel;
 
@@ -40,7 +41,6 @@ void *best_fit(unsigned int size) {
             if (hladam_moznost->size < najlepsia_moznost || najlepsia_moznost == 0) {
                 najlepsia_moznost = hladam_moznost->size;
                 najlepsi_blok = hladam_moznost;
-                printf("X\n");
             }
         }
        hladam_moznost = hladam_moznost->dalsi;
@@ -96,10 +96,105 @@ void *memory_alloc(unsigned int size) {
 
 }
 
+int memory_free(void *valid_ptr){
+
+    HEAD * prehladavac =ukazovatel;
+    //príde mi tam adresa toho payloadu
+
+    HEAD *tmp_pointer = valid_ptr;
+
+    while (1){
+        if (prehladavac->dalsi == tmp_pointer){ // ak pointer čo prehladava nájde ten môj čo potrebujem free tak
+            if(prehladavac->obsadeny < 1){ // ak je predošlý blok prázdny
+           //[ FREE ] [ ] [ ]
+           //   |----------^
+            prehladavac->dalsi = tmp_pointer->dalsi;
+            }
+
+            if(prehladavac->obsadeny == 1){
+                tmp_pointer->obsadeny = 0;
+            }
+        }
+        // ak je prehladavac prazdny
+
+
+
+
+
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+};
+
+#include <time.h>
+void z1_testovac(char *region, char **pointer, int minBlock, int maxBlock, int minMemory, int maxMemory, int testFragDefrag) {
+    srand(time(0));
+    unsigned int allocated = 0;
+    unsigned int mallocated = 0;
+    unsigned int allocated_count = 0;
+    unsigned int mallocated_count = 0;
+    unsigned int i = 0;
+    int random_memory = 0;
+    int random = 0;
+    memset(region, 0, 100000);
+    random_memory = (rand() % (maxMemory-minMemory+1)) + minMemory;
+    memory_init(region + 500, random_memory);
+    if (testFragDefrag) {
+        do {
+            pointer[i] = memory_alloc(8);
+            if (pointer[i])
+                i++;
+        } while (pointer[i]);
+
+    }
+    i = 0;
+    while (allocated <= random_memory-minBlock) {
+        random = (rand() % (maxBlock-minBlock+1)) + minBlock;
+        if (allocated + random > random_memory)
+            continue;
+        allocated += random;
+        allocated_count++;
+        pointer[i] = memory_alloc(random);
+        if (pointer[i]) {
+            i++;
+            mallocated_count++;
+            mallocated += random;
+        }
+    }
+
+    memset(region + 500, 0, random_memory);
+    for (int j = 0; j < 100000; j++) {
+        if (region[j] != 0) {
+            region[j] = 0;
+            printf("Error: Modified memory outside the managed region. index: %d\n",j-500);
+        }
+    }
+    float result = ((float)mallocated_count / allocated_count) * 100;
+    float result_bytes = ((float)mallocated / allocated) * 100;
+    printf("Memory size of %d bytes: allocated %.2f%% blocks (%.2f%% bytes).\n", random_memory, result, result_bytes);
+}
 
 int main() {
+    char region[100000];
+    char* pointer[13000];
+    z1_testovac(region, pointer, 8, 24, 50, 100, 1);
+    z1_testovac(region, pointer, 8, 1000, 10000, 20000, 0);
+    z1_testovac(region, pointer, 8, 35000, 50000, 99000, 0);
+    return 0;
+}
+/*
+int main() {
     char pole[1000];
-    memory_init(pole, 10);
+    //memory_init(pole, 10);
     void *ptr = ukazovatel;
     //memory_init(pole, 10);
 
@@ -120,5 +215,5 @@ int main() {
 
 
 
-
+*/
 
